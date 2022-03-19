0f17d28a66d04083595bf1900abbc7b0fe708ba8
Proloycb <proloychacroborty@gmail.com>
2022-03-19 12:11:45 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <LoadPosts></LoadPosts>
+      <District name="Kishoreganj" specialty= "kishore"></District>
+      <District name="B-Baria" specialty= "jograjati"></District>
+      <District name="Cumilla" specialty= "moyna and moti"></District>
     </div>
   );
 }
 
+const districtStyle = {
+  backgroundColor: 'lightgrey',
+  padding: '20px',
+  margin: '15px'
+
+}
+function District(props) {
+  const[power, setPower] = useState(1);
+  const boostPower= () => setPower(power * 2);
+  return (
+    <div style={districtStyle}>
+      <h2>Name:{props.name}</h2>
+      <p>Specialty:{props.specialty}</p>
+      <h4>Power: {power}</h4>
+      <button onClick={boostPower}>Power of boost</button>
+    </div>
+  );
+}
+
+function LoadPosts() {
+  const [posts, setPosts] = useState([]);
+  useEffect( () => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(res => res.json())
+      .then(data => setPosts(data));
+  }, []);
+  return (
+    <div>
+      <h1>Posts: {posts.length}</h1>
+      {
+        posts.map( post => <Post 
+          title= {post.title} 
+          body= {post.body}
+          key = {post.id}></Post>)
+      }
+    </div>
+  );
+}
+
+function Post (props) {
+  return (
+    <div>
+      <h4>Title: {props.title}</h4>
+      <p><strong>Body:</strong>{props.body}</p>
+    </div>
+  )
+}
 export default App;
